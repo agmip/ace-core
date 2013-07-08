edd76409ea33566449ad04593662060f9de510c0
Christopher Villalobos <cvillalobos@ufl.edu>
2013-07-07 20:08:20 -0400

    Migrated MetadataFilter from Conflate

--- src/main/java/org/agmip/ace/util/MetadataFilter.java
+++ src/main/java/org/agmip/ace/util/MetadataFilter.java
@@ -6,7 +6,7 @@ import java.io.InputStreamReader;
 import java.util.Map;
 import java.util.Set;
 import java.util.concurrent.ConcurrentHashMap;
-import java.util.concurrent.CopyOnWriteArraySet;
+import java.util.concurrent.ConcurrentSkipListSet;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -17,17 +17,19 @@ public enum MetadataFilter {
     INSTANCE;
 
     private final Logger LOG = LoggerFactory.getLogger("org.agmip.ace.util.MetadataFilter");
-    private final Set<String> metadata           = new CopyOnWriteArraySet<>();
-    private final Set<String> required           = new CopyOnWriteArraySet<>();
-    private final Set<String> indexed            = new CopyOnWriteArraySet<>();
-    private final Set<String> restricted         = new CopyOnWriteArraySet<>();
-    private final Set<String> suggested          = new CopyOnWriteArraySet<>();
-    private final Set<String> export             = new CopyOnWriteArraySet<>();
-    private final Set<String> restrictedexport   = new CopyOnWriteArraySet<>();
-    private final Set<String> norestrictedexport = new CopyOnWriteArraySet<>();
+    private final Set<String> metadata           = new ConcurrentSkipListSet<>();
+    private final Set<String> required           = new ConcurrentSkipListSet<>();
+    private final Set<String> indexed            = new ConcurrentSkipListSet<>();
+    private final Set<String> restricted         = new ConcurrentSkipListSet<>();
+    private final Set<String> suggested          = new ConcurrentSkipListSet<>();
+    private final Set<String> export             = new ConcurrentSkipListSet<>();
+    private final Set<String> restrictedexport   = new ConcurrentSkipListSet<>();
+    private final Set<String> norestrictedexport = new ConcurrentSkipListSet<>();
     private final Map<String, Integer> weights   = new ConcurrentHashMap<>();
 
-    MetadataFilter() {
+    MetadataFilter() {}
+    
+    public void initialize() {
         InputStream filter = getClass().getClassLoader().getResourceAsStream("metadata_filter.csv");
         loadFromEmbeddedCSV(filter);
     }
--- src/test/java/org/agmip/ace/util/MetadataFilterTest.java
+++ src/test/java/org/agmip/ace/util/MetadataFilterTest.java
@@ -1,12 +1,20 @@
 package org.agmip.ace.util;
 
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Before;
 import org.junit.Test;
-import static org.junit.Assert.*;
 
 public class MetadataFilterTest {
+    @Before
+    public void setup() {
+        MetadataFilter.INSTANCE.initialize();
+    }
+    
     @Test
     public void checkExport() {
         assertTrue(MetadataFilter.INSTANCE.getExportMetadata().contains("crid"));
-        assertFalse(MetadataFilter.INSTANCE.getRestrictedNoExportMetadata().contains("nothign"));
+        assertFalse(MetadataFilter.INSTANCE.getRestrictedNoExportMetadata().contains("invalid"));
     }
 }
